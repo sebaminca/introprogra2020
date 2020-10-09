5fedd6c761d614fd58eeb5780448d42965a239a5
sebaminca@gmail.com <sebaminca@gmail.com>
2020-10-09 20:16:48 -0300

    mi primer commit

---
+++ clase2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
